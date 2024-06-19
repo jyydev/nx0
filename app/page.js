5863c6fda182de508f8e703b94bf9d48364bc751
Y <jydev@yahoo.com>
2024-06-19 11:31:50 +0800

    t

--- app/page.js
+++ app/page.js
@@ -1,11 +1,20 @@
+import getAccs from "@/fn/accs";
+import get from "@/fn/get";
 import toPrecision from "toprecision";
 export const dynamic = "force-dynamic";
 
-export default function Home() {
+export default async function Home() {
   let n = toPrecision(1.2345, 3);
+  let accs = getAccs();
+  let [pmR] = await Promise.all([get("/papi/v1/account", {}, accs)]);
+  console.log(pmR);
+
   return (
     <div>
-      NEXTJS: tt:{process.env.tt}, n:{n}, 11
+      <div>
+        NEXTJS: tt:{process.env.tt}, n:{n}, 11
+      </div>
+      <div>aa {pmR.s2.uniMMR} bb</div>
     </div>
   );
 }
